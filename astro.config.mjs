1a4057f8693cf493af3962122521531095d40604
Cas74l4n <63763425+Cas74l4n@users.noreply.github.com>
2023-12-25 17:41:58 -0600

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://github.com/Cas74l4n/gabriel.github.io',
+    site: 'https://github.com/Cas74l4n',
     base: '/gabriel.github.io',
 });
