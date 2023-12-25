0969406002e0461843c5fab448a3b103a7ad3e3d
Cas74l4n <catelan.15@outlook.es>
2023-12-25 17:39:03 -0600

    intento

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://gabriel.github.io',
+    site: 'https://github.com/Cas74l4n/gabriel.github.io',
     base: '/gabriel.github.io',
 });
